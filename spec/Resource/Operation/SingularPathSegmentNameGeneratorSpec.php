55ae07a5529953b7716ec31b235c0b0b10fe3533
Anthonius Munthi <me@itstoni.com>
2021-09-13 17:39:58 +0800

    cs-fix

--- spec/Resource/Operation/SingularPathSegmentNameGeneratorSpec.php
+++ spec/Resource/Operation/SingularPathSegmentNameGeneratorSpec.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the OpenSID project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
 namespace spec\OpenSID\Resource\Operation;
 
 use ApiPlatform\Core\Operation\PathSegmentNameGeneratorInterface;
@@ -8,19 +17,19 @@ use PhpSpec\ObjectBehavior;
 
 class SingularPathSegmentNameGeneratorSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+    public function it_is_initializable()
     {
         $this->shouldHaveType(SingularPathSegmentNameGenerator::class);
     }
 
-    function it_should_implement_path_segment_name_generator_interface()
+    public function it_should_implement_path_segment_name_generator_interface()
     {
         $this->shouldImplement(PathSegmentNameGeneratorInterface::class);
     }
 
     public function it_should_returns_valid_segment_name()
     {
-        $name = "artikel";
+        $name = 'artikel';
 
         $this->getSegmentName($name, true)->shouldReturn('artikel');
         $this->getSegmentName($name, false)->shouldReturn('artikel');
